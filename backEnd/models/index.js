cc2de2631ce23ced7551a85bcffa3107d05e2b33
codingmohamed <cloudchronicles92@gmail.com>
2022-01-18 12:22:44 +0000

    POST and GET ALL done

--- backEnd/models/index.js
+++ backEnd/models/index.js
@@ -1,6 +1,14 @@
 import query from "../db/index.js";
 
 export async function getPosts() {
-  const data = await query(`SELECT * FROM library`);
-  return data.rows;
+      const data = await query(`SELECT * FROM library;`);
+      return data.rows;
+}
+
+export async function createPost(week, day, topic, link) {
+      const data = await query(
+            `INSERT INTO library (week, day, topic, link) VALUES ($1, $2, $3, $4) RETURNING *`,
+            [week, day, topic, link]
+      );
+      return data.rows;
 }
--- backEnd/routes/users.js
+++ backEnd/routes/users.js
@@ -1,9 +1,29 @@
 import express from "express";
 const router = express.Router();
+import { getPosts, createPost } from "../models/index.js";
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.json({ message: "I wish we had some information to give you ☹️" });
-});
+// /* GET users listing. */
+// router.get("/", function (req, res, next) {
+//       res.json({ message: "I wish we had some information to give you ☹️" });
+// });
 
 export default router;
+
+//GET all posts
+router.get("/", async function (req, res) {
+      const allPosts = await getPosts();
+      res.json({
+            success: true,
+            payload: allPosts,
+      });
+});
+
+// Create new post:
+router.post("/", async function (req, res) {
+      const { week, day, topic, link } = req.body;
+      const newPost = await createPost(week, day, topic, link);
+      res.json({
+            success: true,
+            payload: newPost,
+      });
+});
