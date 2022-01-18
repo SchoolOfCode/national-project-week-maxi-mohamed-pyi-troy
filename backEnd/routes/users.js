72187de428e0dc039504e2c84df293c554604ed0
codingmohamed <cloudchronicles92@gmail.com>
2022-01-18 15:18:52 +0000

    final backend

--- backEnd/routes/users.js
+++ backEnd/routes/users.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
 import {
-  getPosts,
-  createPost,
-  deletePost,
-  updatePost,
+      getPosts,
+      createPost,
+      deletePost,
+      updatePost,
+      getPostID,
 } from "../models/index.js";
 
 // /* GET users listing. */
@@ -16,43 +17,54 @@ export default router;
 
 //GET all posts
 router.get("/", async function (req, res) {
-  const allPosts = await getPosts();
-  res.json({
-    success: true,
-    payload: allPosts,
-  });
+      const allPosts = await getPosts();
+      res.json({
+            success: true,
+            payload: allPosts,
+      });
 });
 
 // Create new post:
 router.post("/", async function (req, res) {
-  const { week, day, topic, link } = req.body;
-  const newPost = await createPost(week, day, topic, link);
-  res.json({
-    success: true,
-    payload: newPost,
-  });
+      const { week, day, topic, link } = req.body;
+      const newPost = await createPost(week, day, topic, link);
+      res.json({
+            success: true,
+            payload: newPost,
+      });
 });
 
 // Delete post:
 
 router.delete("/:id", async function (req, res) {
-  const { id } = req.params;
-  const data = await deletePost(id);
-  const result = await getPosts();
-  res.json({
-    success: true,
-    payload: result,
-  });
+      const { id } = req.params;
+      const data = await deletePost(id);
+      const result = await getPosts();
+      res.json({
+            success: true,
+            payload: result,
+      });
 });
 
 // Update post:
 
 router.patch("/:id", async function (req, res) {
-  const { id } = req.params;
-  const { week, day, topic, link } = req.body;
-  const data = await updatePost(id, week, day, topic, link);
-  res.json({
-    success: true,
-    payload: data,
-  });
+      const { id } = req.params;
+      const { week, day, topic, link } = req.body;
+      const data = await updatePost(id, week, day, topic, link);
+      res.json({
+            success: true,
+            payload: data,
+      });
+});
+
+// GET by ID
+
+router.get("/:id", async function (req, res) {
+      const { id } = req.params;
+      const postByID = await getPostID(id);
+      res.json({
+            success: true,
+            payload: postByID,
+      });
 });
