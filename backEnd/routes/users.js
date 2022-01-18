88e44feb64b3e31c276dda35802ea7f760338d8d
Maximiliano Sinopoli <m_sinopoli@hotmail.com>
2022-01-18 14:23:20 +0000

    Route patch done

--- backEnd/routes/users.js
+++ backEnd/routes/users.js
@@ -1,6 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { getPosts, createPost } from "../models/index.js";
+import {
+  getPosts,
+  createPost,
+  deletePost,
+  updatePost,
+} from "../models/index.js";
 
 // /* GET users listing. */
 // router.get("/", function (req, res, next) {
@@ -11,19 +16,43 @@ export default router;
 
 //GET all posts
 router.get("/", async function (req, res) {
-      const allPosts = await getPosts();
-      res.json({
-            success: true,
-            payload: allPosts,
-      });
+  const allPosts = await getPosts();
+  res.json({
+    success: true,
+    payload: allPosts,
+  });
 });
 
 // Create new post:
 router.post("/", async function (req, res) {
-      const { week, day, topic, link } = req.body;
-      const newPost = await createPost(week, day, topic, link);
-      res.json({
-            success: true,
-            payload: newPost,
-      });
+  const { week, day, topic, link } = req.body;
+  const newPost = await createPost(week, day, topic, link);
+  res.json({
+    success: true,
+    payload: newPost,
+  });
+});
+
+// Delete post:
+
+router.delete("/:id", async function (req, res) {
+  const { id } = req.params;
+  const data = await deletePost(id);
+  const result = await getPosts();
+  res.json({
+    success: true,
+    payload: result,
+  });
+});
+
+// Update post:
+
+router.patch("/:id", async function (req, res) {
+  const { id } = req.params;
+  const { week, day, topic, link } = req.body;
+  const data = await updatePost(id, week, day, topic, link);
+  res.json({
+    success: true,
+    payload: data,
+  });
 });
