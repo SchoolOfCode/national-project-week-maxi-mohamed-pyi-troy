71af6a7c7e5f99c2d1d823b6f00dcc5e9937fd3f
codingmohamed <cloudchronicles92@gmail.com>
2022-01-19 15:33:54 +0000

    working on submit form

--- frontEnd/socdashboard/src/components/Library/index.js
+++ frontEnd/socdashboard/src/components/Library/index.js
@@ -5,37 +5,36 @@ import NewResourceForm from "../NewResourceForm";
 import React, { useState, useEffect } from "react";
 
 const Library = () => {
-  const [posts, setPosts] = useState([]);
+      const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    async function getAllTasks() {
-      const response = await fetch("http://localhost:4000/users/");
-      const data = await response.json();
-      setPosts(data.payload);
-    }
-    getAllTasks();
-  }, []);
+      useEffect(() => {
+            async function getAllTasks() {
+                  const response = await fetch("http://localhost:4000/users/");
+                  const data = await response.json();
+                  setPosts(data.payload);
+            }
+            getAllTasks();
+      }, []);
 
-  console.log(posts);
+      console.log(posts);
 
-  return (
-    <div className="LibraryContainer">
-      <SearchBar />
-      <button>Add to library</button>
-      <NewResourceForm />
-      {posts.map((item) => {
-        return (
-          <LibraryResult
-            key={item.topic}
-            title={item.topic}
-            week={item.week}
-            day={item.day}
-            link={item.link}
-          />
-        );
-      })}
-    </div>
-  );
+      return (
+            <div className="LibraryContainer">
+                  <SearchBar />
+                  <NewResourceForm />
+                  {posts.map((item) => {
+                        return (
+                              <LibraryResult
+                                    key={item.topic}
+                                    title={item.topic}
+                                    week={item.week}
+                                    day={item.day}
+                                    link={item.link}
+                              />
+                        );
+                  })}
+            </div>
+      );
 };
 
 export default Library;
--- frontEnd/socdashboard/src/components/NewResourceForm/index.js
+++ frontEnd/socdashboard/src/components/NewResourceForm/index.js
@@ -1,17 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewResourceForm = () => {
-  return (
-    <div className="newResourceFormContainer">
-      <form>
-        <input type="number" placeholder="Week number?" />
-        <input type="number" placeholder="Day number?" />
-        <input type="text" placeholder="Topic Description" />
-        <input type="url" placeholder="Link" />
-        <button>Submit</button>
-      </form>
-    </div>
-  );
+      const [newPost, setNewPost] = useState({});
+      const [week, setWeek] = useState("");
+      const [day, setDay] = useState("");
+      const [topic, setTopic] = useState("");
+      const [link, setLink] = useState("");
+
+      function getWeek(week) {
+            setWeek(week);
+      }
+
+      function getTopic(topic) {
+            setTopic(topic);
+      }
+
+      function getDay(day) {
+            setDay(day);
+      }
+
+      function getLink(link) {
+            setLink(link);
+      }
+
+      console.log(week);
+
+      async function onSubmitForm(event) {
+            event.preventDefault();
+            const body = { newPost };
+            const response = await fetch("http://localhost:4000/users/", {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(newPost),
+            });
+      }
+      return (
+            <div className="newResourceFormContainer">
+                  <form>
+                        <input
+                              type="number"
+                              placeholder="Week number?"
+                              onChange={(event) => getWeek(event.target.value)}
+                        />
+                        <input
+                              type="number"
+                              placeholder="Day number?"
+                              onChange={(event) => getDay(event.target.value)}
+                        />
+                        <input
+                              type="text"
+                              placeholder="Topic Description"
+                              onChange={(event) => getTopic(event.target.value)}
+                        />
+                        <input
+                              type="url"
+                              placeholder="Link"
+                              onChange={(event) => getLink(event.target.value)}
+                        />
+                        <button>Submit</button>
+                  </form>
+            </div>
+      );
 };
 
 export default NewResourceForm;
