7677797e60421ba56a102f86a1d14986b101ab55
codingmohamed <cloudchronicles92@gmail.com>
2022-01-20 11:46:33 +0000

    submit form working

--- src/components/NewResourceForm/index.js
+++ src/components/NewResourceForm/index.js
@@ -1,67 +1,70 @@
 import React from "react";
 import "./index.css";
+import { useState, useEffect } from "react";
 
 const NewResourceForm = () => {
-  // const [newPost, setNewPost] = useState({});
-  // const [week, setWeek] = useState("");
-  // const [day, setDay] = useState("");
-  // const [topic, setTopic] = useState("");
-  // const [link, setLink] = useState("");
+      const [week, setWeek] = useState(0);
+      const [day, setDay] = useState(0);
+      const [topic, setTopic] = useState("");
+      const [link, setLink] = useState("");
 
-  // function getWeek(week) {
-  //   setWeek(week);
-  // }
+      function getWeek(week) {
+            setWeek(week);
+      }
 
-  // function getTopic(topic) {
-  //   setTopic(topic);
-  // }
+      function getTopic(topic) {
+            setTopic(topic);
+      }
 
-  // function getDay(day) {
-  //   setDay(day);
-  // }
+      function getDay(day) {
+            setDay(day);
+      }
 
-  // function getLink(link) {
-  //   setLink(link);
-  // }
+      function getLink(link) {
+            setLink(link);
+      }
 
-  // console.log(week);
-
-  // async function onSubmitForm(event) {
-  //   event.preventDefault();
-  //   const body = { newPost };
-  //   const response = await fetch("http://localhost:4000/users/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify(newPost),
-  //   });
-  // }
-  return (
-    <div className="submit-form">
-      <form>
-        <input
-          type="number"
-          placeholder="Week number?"
-          // onChange={(event) => getWeek(event.target.value)}
-        />
-        <input
-          type="number"
-          placeholder="Day number?"
-          // onChange={(event) => getDay(event.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Topic Description"
-          // onChange={(event) => getTopic(event.target.value)}
-        />
-        <input
-          type="url"
-          placeholder="Link"
-          // onChange={(event) => getLink(event.target.value)}
-        />
-        <button className="submit-button">Submit</button>
-      </form>
-    </div>
-  );
+      async function onSubmitForm(event) {
+            event.preventDefault();
+            const body = { week: week, link: link, topic: topic, day: day };
+            const response = await fetch(
+                  "https://schoolofcodelibrary.herokuapp.com/users/",
+                  {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify(body),
+                  }
+            );
+            const data = await response.json();
+            return data;
+      }
+      return (
+            <div className="submit-form">
+                  <form onSubmit={(e) => onSubmitForm(e)}>
+                        <input
+                              type="number"
+                              placeholder="Week number?"
+                              onChange={(event) => getWeek(event.target.value)}
+                        />
+                        <input
+                              type="number"
+                              placeholder="Day number?"
+                              onChange={(event) => getDay(event.target.value)}
+                        />
+                        <input
+                              type="text"
+                              placeholder="Topic Description"
+                              onChange={(event) => getTopic(event.target.value)}
+                        />
+                        <input
+                              type="text"
+                              placeholder="Link"
+                              onChange={(event) => getLink(event.target.value)}
+                        />
+                        <button className="submit-button">Submit</button>
+                  </form>
+            </div>
+      );
 };
 
 export default NewResourceForm;
