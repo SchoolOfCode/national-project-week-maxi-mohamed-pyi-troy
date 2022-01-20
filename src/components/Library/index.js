330048c84f47d82838864a4cd19445417df4e07d
Maximiliano Sinopoli <m_sinopoli@hotmail.com>
2022-01-20 22:05:25 +0000

    Filter

--- src/components/Library/index.js
+++ src/components/Library/index.js
@@ -1,48 +1,61 @@
 import LibraryResult from "../LibraryResult";
 import SearchBar from "../SearchBar";
 import NewResourceForm from "../NewResourceForm";
+
 import React, { useState, useEffect } from "react";
 import "./index.css";
 
 const Library = ({ user }) => {
-  const [fetchPosts, setFetchPosts] = useState(true);
   const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function onChange(e) {
+    setSearchTerm(e.target.value);
+    console.log(searchTerm);
+  }
+
   useEffect(() => {
-    console.log("Lib");
     async function getAllTasks() {
       const response = await fetch(
         "https://schoolofcodelibrary.herokuapp.com/users/"
       );
       const data = await response.json();
       setPosts(data.payload);
-      setFetchPosts(false);
-      console.log(data.payload);
     }
-    if (fetchPosts) {
-      getAllTasks();
-    }
-  }, [fetchPosts]);
+    getAllTasks();
+  }, [posts]);
   return (
     <div className="LibraryContainer">
-      <SearchBar />
+      <SearchBar onChange={onChange} />
+
       <div className="submit-results">
-        {user === "admin" && <NewResourceForm setFetchPosts={setFetchPosts} />}
+        {user === "admin" && <NewResourceForm setPosts={setPosts} />}
       </div>
-      {posts.map((item) => {
-        return (
-          <LibraryResult
-            setFetchPosts={setFetchPosts}
-            key={item.id}
-            title={item.topic}
-            week={item.week}
-            day={item.day}
-            link={item.link}
-            id={item.id}
-            user={user}
-          />
-        );
-      })}
+      {posts
+        .filter((item) => {
+          if (searchTerm == "") {
+            return item;
+          } else if (
+            item.topic.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+          ) {
+            return item;
+          }
+        })
+        .map((item) => {
+          return (
+            <LibraryResult
+              setPosts={setPosts}
+              id={item.id}
+              key={item.topic}
+              title={item.topic}
+              week={item.week}
+              day={item.day}
+              link={item.link}
+            />
+          );
+        })}
     </div>
   );
 };
+
 export default Library;
