03aca24cc19189914d0a089d3c5c5ec630539931
burmanp <kohein@gmail.com>
2022-01-20 16:14:12 +0000

    fetch loop resolved

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,38 +3,39 @@ import "./styles.css";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-      return (
-            <div className="navbarContainer">
-                  <nav>
-                        <ul className="listContainer">
-                              <div className="nav-logos">
-                                    <Link to="/main">
-                                          <li>
-                                                <img
-                                                      src="https://media-public.canva.com/cnmeE/MADE1RcnmeE/2/t.png"
-                                                      alt="libraryLogo"
-                                                ></img>
-                                          </li>
-                                    </Link>
-                                    <Link to="/buddies">
-                                          <li>
-                                                <img
-                                                      src="https://media-public.canva.com/8wuZ4/MAESjo8wuZ4/1/t.png"
-                                                      alt="studyBuddyLogo"
-                                                ></img>
-                                          </li>
-                                    </Link>
-                                    <li>
-                                          <img
-                                                src="https://media-public.canva.com/HHe5s/MAD2U9HHe5s/1/t.png"
-                                                alt="starIcon"
-                                          ></img>
-                                    </li>
-                              </div>
-                        </ul>
-                  </nav>
-            </div>
-      );
+  return (
+    <div className="navbarContainer">
+      <nav>
+        <ul className="listContainer">
+          <div className="nav-logos">
+            <Link to="/main">
+              <li>
+                <img
+                  src="https://media-public.canva.com/cnmeE/MADE1RcnmeE/2/t.png"
+                  alt="libraryLogo"
+                ></img>
+              </li>
+            </Link>
+            <Link to="/buddies">
+              <li>
+                <img
+                  src="https://media-public.canva.com/8wuZ4/MAESjo8wuZ4/1/t.png"
+                  alt="studyBuddyLogo"
+                ></img>
+              </li>
+            </Link>
+
+            <li>
+              <img
+                src="https://media-public.canva.com/HHe5s/MAD2U9HHe5s/1/t.png"
+                alt="starIcon"
+              ></img>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
